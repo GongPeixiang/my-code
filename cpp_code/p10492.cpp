c0f9528a00f83b8066b0434b3bcce1504d121794
GongPeixiang <iwanttogotoamerica@163.com>
2026-05-23 13:19:48 +0800

    small_revise

--- cpp_code/p10492.cpp
+++ cpp_code/p10492.cpp
@@ -1,36 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-vector<array<array<bool, 4>, 4>> sched;
-constexpr array<int, 9> dx = { 0, 0, 0, 0, 0, 1, 2, -1, -2 };
-constexpr array<int, 9> dy = { 0, 1, 2, -1, -2, 0, 0, 0, 0 };
-vector<int> memo;
+constexpr int MAXN = 365;
+constexpr int dx[9] = {0, 0, 0, 0, 0, 1, 2, -1, -2};
+constexpr int dy[9] = {0, 1, 2, -1, -2, 0, 0, 0, 0};
 
-auto idx_memo = [](int d, int cx, int cy, int tl, int tr, int bl, int br) {
-    return ((((((d * 4 + cx) * 4 + cy) * 8 + tl) * 8 + tr) 
-    * 8 + bl) * 8 + br);
-};
+int N, sched[MAXN][4][4];
+int memo[MAXN][4][4][8][8][8][8];
 
-inline bool check(int day, int cx, int cy) {
+bool check(int d, int cx, int cy) {
     for (int i = cx; i <= cx + 1; ++i) {
         for (int j = cy; j <= cy + 1; ++j) {
-            if (sched[day][i][j]) 
+            if (sched[d][i][j]) 
                 return false;
         }
     }
     return true;
 }
 
-bool dfs(int day, int cx, int cy, int tl, int tr, int bl, int br) {
-    int idx = idx_memo(day, cx, cy, tl, tr, bl, br);
-    if (memo[idx] != -1) 
-        return memo[idx];
-    if (!check(day, cx, cy)) 
+bool dfs(int d, int cx, int cy, int tl, int tr, int bl, int br) {
+    if (memo[d][cx][cy][tl][tr][bl][br] != -1) 
+        return memo[d][cx][cy][tl][tr][bl][br];
+    if (!check(d, cx, cy)) 
         return false;
     if (tl >= 7 || tr >= 7 || bl >= 7 || br >= 7) 
         return false;
-    if (day == N - 1)
+    if (d == N - 1) 
         return true;
     bool res = false;
     for (int i = 0; i < 9; ++i) {
@@ -41,19 +36,18 @@ bool dfs(int day, int cx, int cy, int tl, int tr, int bl, int br) {
         int ntr = (ncx == 0 && ncy == 2) ? 0 : tr + 1;
         int nbl = (ncx == 2 && ncy == 0) ? 0 : bl + 1;
         int nbr = (ncx == 2 && ncy == 2) ? 0 : br + 1;
-        res |= dfs(day + 1, ncx, ncy, ntl, ntr, nbl, nbr);
+        res |= dfs(d + 1, ncx, ncy, ntl, ntr, nbl, nbr);
     }
-    memo[idx] = res;
+    memo[d][cx][cy][tl][tr][bl][br] = res;
     return res;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(nullptr)->sync_with_stdio(false);
     while (1) {
         cin >> N;
-        if (!N) break;
-        sched.resize(N);
+        if (!N) 
+            break;
         for (int d = 0; d < N; ++d) {
             for (int i = 0; i < 4; ++i) {
                 for (int j = 0; j < 4; ++j) {
@@ -61,9 +55,7 @@ int main() {
                 }
             }
         }
-        // std::array和vector用memset初始化需要arr.data()
-        memo.resize(N * 4 * 4 * 8 * 8 * 8 * 8);
-        memset(memo.data(), 0xff, memo.size() * sizeof(int));
+        memset(memo, 0xff, sizeof(memo));
         bool ans = dfs(0, 1, 1, 1, 1, 1, 1);
         cout << ans << '\n';
     }
