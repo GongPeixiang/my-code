0114949cb7a663527bd3da21c979ada4758982cb
GongPeixiang <iwanttogotoamerica@163.com>
2026-09-08 23:52:15 +0800

    bi_add

--- yoj/t291.cpp
+++ yoj/t291.cpp
@@ -1,91 +1,92 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string a, b, result;
+const int N = 20005;
 
-// 正数相加
-void add(string &a, string &b) {
-    int len = max(a.size(), b.size());
-    result.resize(len);
-    if (a.size() < len) 
-        a.insert(a.begin(), len - a.size(), '0');
-    if (b.size() < len) 
-        b.insert(b.begin(), len - b.size(), '0');
-    int carry = 0;
-    for (int i = len - 1; i >= 0; i--) {
-        int x = a[i] - '0', y = b[i] - '0';
-        int tmp = x + y + carry;
-        result[i] = (tmp % 10) + '0';
-        carry = tmp / 10;
+char s, a[N], b[N], ans[N], tmp[N];
+int x[N], y[N], res[N];
+
+void add(bool flg) { // flg for '-'
+    int la = strlen(a), lb = strlen(b);
+    for (int i = 0; i < la; i++) x[i] = a[la-1-i] - '0';
+    for (int j = 0; j < lb; j++) y[j] = b[lb-1-j] - '0';
+    int l = max(la, lb);
+    for (int i = 0; i < l; i++) {
+        res[i] += x[i] + y[i];
+        res[i+1] += res[i] / 10;
+        res[i] %= 10;
     }
-    if (carry) 
-        result = '1' + result;
+    while (res[l] == 0 && l > 0) l--;
+    int cnt = 0;
+    if (flg) ans[cnt++] = '-';
+    for (int i = l; i >= 0; i--) ans[cnt++] = (char)(res[i] + '0');
+    ans[cnt++] = '\0';
 }
 
-// 正数相减， 但是minus是C++的一个库函数
-void substract(string &a, string &b) {
-    bool is_minus = false;
-    int len = max(a.size(), b.size());
-    result.resize(len);
-    if (a.size() < len || (a.size() == b.size() && a < b)) {
-        swap(a, b);
-        is_minus = true;
+void subs() {
+    int la = strlen(a), lb = strlen(b);
+    bool flg = 0;
+    if (la < lb || la==lb && strcmp(a,b)<0) {
+        strcpy(tmp, a);  strcpy(a, b);  strcpy(b, tmp);
+        flg = 1;
     }
-    b.insert(b.begin(), len - b.size(), '0');
-    int borrow = 0;
-    for (int i = len - 1; i >= 0; i--) {
-        int x = a[i] - '0' - borrow, y = b[i] - '0';
-        borrow = x >= y ? 0 : 1;
-        int tmp = x + borrow * 10 - y;
-        result[i] = tmp + '0';
+    la = strlen(a), lb = strlen(b);
+    for (int i = 0; i < la; i++) x[i] = a[la-1-i] - '0';
+    for (int j = 0; j < lb; j++) y[j] = b[lb-1-j] - '0';
+    for (int i = 0; i < la; i++) {
+        res[i] += x[i] - y[i];
+        if (res[i] < 0) {
+            res[i+1] -= 1;
+            res[i] += 10;
+        }
     }
-    while (result[0] == '0') 
-        result.erase(0, 1);
-    if (is_minus) 
-        result = '-' + result;
+    int l = la;
+    while (res[l] == 0 && l > 0) l--;
+    int cnt = 0;
+    if (flg) ans[cnt++] = '-';
+    for (int i = l; i >= 0; i--) ans[cnt++] = (char)(res[i] + '0');
+    ans[cnt++] = '\0';
+}
+
+inline void rm_neg(char* str) {
+    int l = strlen(str);
+    memmove(str, str + 1, (l-1)*sizeof(char));
+    str[l-1] = '\0';
 }
 
 int main() {
-    char sign;
-    cin >> sign >> a >> b;
-    if (sign == '+') {
-        if (a[0] != '-' && b[0] != '-') {
-            add(a, b);
-        }
-        else if (a[0] != '0' && b[0] == '-') {
-            b.erase(0, 1);
-            substract(a, b);
-        }
-        else if (a[0] == '-' && b[0] != '-') {
-            a.erase(0, 1);
-            substract(b, a);
+    scanf("%c%s%s", &s, a, b);
+    if (s == '+') {
+        if (a[0]!='-' && b[0]!='-') {
+            add(0);
+        } else if (a[0]!='-' && b[0]=='-') {
+            rm_neg(b);
+            subs();
+        } else if (a[0]=='-' && b[0]!='-') {
+            rm_neg(a);
+            strcpy(tmp, a); strcpy(a, b); strcpy(b, tmp);
+            subs();
         }
         else {
-            a.erase(0, 1); 
-            b.erase(0, 1);
-            add(a, b);
-            result = '-' + result;
+            rm_neg(a);  rm_neg(b);
+            add(1);
         }
     }
     else {
-        if (a[0] != '-' && b[0] != '-') {
-            substract(a, b);
-        }
-        else if (a[0] != '-' && b[0] == '-') {
-            b.erase(0, 1);
-            add(a, b);
-        }
-        else if (a[0] == '-' && b[0] != '-') {
-            a.erase(0, 1);
-            add(a, b);
-            result = '-' + result;
-        }
-        else {
-            a.erase(0, 1); 
-            b.erase(0, 1);
-            substract(b, a);
+        if (a[0]!='-' && b[0]!='-') {
+            subs();
+        } else if (a[0]!='-' && b[0]=='-') {
+            rm_neg(b);
+            add(0);
+        } else if (a[0]=='-' && b[0]!='-') {
+            rm_neg(a);
+            add(1);
+        } else {
+            rm_neg(a); rm_neg(b);
+            strcpy(tmp, a); strcpy(a, b); strcpy(b, tmp);
+            subs();
         }
     }
-    cout << result << '\n';
+    printf("%s\n", ans);
     return 0;
 }
